9c92f722b2719a435e073e18db47817f016cac35
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-09-20 00:26:36 +0300

    chore: bump all versions to 0.1.1

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
           # error.
           webui = pkgs.stdenvNoCC.mkDerivation {
             pname = "otelview-webui";
-            version = "0.1.0";
+            version = "0.1.1";
             src = lib.cleanSourceWith {
               src = ./ui;
               filter = path: type:
@@ -106,7 +106,7 @@
           commonArgs = {
             inherit src;
             pname = "otelview";
-            version = "0.1.0";
+            version = "0.1.1";
             strictDeps = true;
             nativeBuildInputs = [ pkgs.protobuf ];
             PROTOC = "${pkgs.protobuf}/bin/protoc";
