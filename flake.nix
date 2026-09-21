28a7680a953c21eb0c5c074db216ffc3c183953d
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-09-21 11:06:34 +0300

    chore: release 0.2.1

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
           # error.
           webui = pkgs.stdenvNoCC.mkDerivation {
             pname = "otelview-webui";
-            version = "0.2.0";
+            version = "0.2.1";
             src = lib.cleanSourceWith {
               src = ./ui;
               filter = path: type:
@@ -109,7 +109,7 @@
           commonArgs = {
             inherit src;
             pname = "otelview";
-            version = "0.2.0";
+            version = "0.2.1";
             strictDeps = true;
             nativeBuildInputs = [ pkgs.protobuf ];
             PROTOC = "${pkgs.protobuf}/bin/protoc";
